c87ab32453cb36ae5de1416f04a267b1e8ec1e91
44046053 <balamurugan.guruswamy@hsbc.com>
2018-08-20 11:41:57 +0100

    Models changed!

--- src/main/scala/com/poc/sample/Models.scala
+++ src/main/scala/com/poc/sample/Models.scala
@@ -5,13 +5,12 @@ import com.fasterxml.jackson.annotation.{JsonInclude, JsonProperty}
 
 object Models {
 
-  case class CIAMaterialConfig(appName: String, environment: String, sourceName: String, kerberosPrincipal: String, kerberosKeyTabLocation: String, esStatusIndicator: Boolean, esHost: String,
-                               esPort: String, esIndex: String, schemaDirectory: String, attunityCDCIndicator: Boolean, cdcJournalControlFields: String,
-                               createBaseTable: Boolean, incrementalHiveTableExist: Boolean, seqColumn: String, headerOperation: String, deleteIndicator: String,
-                               beforeImageIndicator: String, mandatoryMetaData: String, overrideIndicator: String, materialConfigs: List[MaterialConfig]
+  case class CIAMaterialConfig(appName: String, environment: String, clusterName: String, sourceName: String, kerberosPrincipal: String, kerberosKeyTabLocation: String, esStatusIndicator: Boolean, esIndex: String, schemaDirectory: String, attunityCDCIndicator: Boolean, cdcJournalControlFields: String,
+                               createBaseTable: Boolean, createBaseTableFromScooped: Boolean, incrementalHiveTableExist: Boolean, seqColumn: String, headerOperation: String, deleteIndicator: String,
+                               beforeImageIndicator: String, mandatoryMetaData: String, overrideIndicator: Boolean, materialConfigs: List[MaterialConfig]
                               )
 
-  case class MaterialConfig(hiveDatabase: String, baseTableName: String, createBaseTable: Boolean, incrementalHiveTableExist: Boolean, incrementalTableName: String,
+  case class MaterialConfig(hiveDatabase: String, baseTableName: String, createBaseTable: Boolean, createBaseTableFromScooped: Boolean, incrementalHiveTableExist: Boolean, incrementalTableName: String,
                             pathToLoad: String, attunityUnpackedPath: String, attunityUnpackedArchive: String,
                             processedPathToMove: String, uniqueKeyList: String, partitionColumns: String, seqColumn: String,
                             headerOperation: String, deleteIndicator: String, beforeImageIndicator: String, mandatoryMetaData: String)
@@ -19,9 +18,9 @@ object Models {
   case class Item(id: String, name: String)
 
   case class CIANotification(sourceName: String, componentName: String, hiveDatabase: String, baseTableName: String, incrementalPathLocation: String, latestTimestamp: String, currentTimestamp: String, noOfRecords: Long, timeTaken: String) {
-    override def toString = "Source Name = " + sourceName + "Component Name =" + componentName + "Hive database = " + hiveDatabase + "Base Table name = " + baseTableName +
-      "Incremental Path =" + incrementalPathLocation + "Latest timestamp in Records =" + latestTimestamp + "Current Timestamp = " + currentTimestamp +
-      "No of Records " + noOfRecords + "Time taken " + timeTaken
+    override def toString = "Source Name = " + sourceName + " Component Name =" + componentName + " Hive database = " + hiveDatabase + " Base Table name = " + baseTableName +
+      " Incremental Path = " + incrementalPathLocation + " Latest timestamp in Records = " + latestTimestamp + " Current Timestamp = " + currentTimestamp +
+      " No of Records " + noOfRecords + " Time taken " + timeTaken
   }
 
   case class AvroSchema(@JsonProperty("type") typz: String, name: String, namespace: String, fields: Array[Fields])
